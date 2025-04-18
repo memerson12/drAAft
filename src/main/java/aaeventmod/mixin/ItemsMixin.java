dd38ba9bb6320ccb32749e5d70e9a50339e6b5dc
PacManMVC <maxim.vancompernolle@gmail.com>
2025-04-17 21:25:08 -0400

    make elytra durability really high

---
+++ src/main/java/aaeventmod/mixin/ItemsMixin.java
@@ -0,0 +1,17 @@
+package aaeventmod.mixin;
+
+import net.minecraft.item.Items;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.ModifyArg;
+import org.spongepowered.asm.mixin.injection.Slice;
+
+@Mixin(Items.class)
+public class ItemsMixin {
+    @ModifyArg(method = "<clinit>",
+            slice = @Slice(from = @At(value = "CONSTANT", args = "stringValue=elytra")),
+            at = @At(value = "INVOKE", target = "Lnet/minecraft/item/Item$Settings;maxDamage(I)Lnet/minecraft/item/Item$Settings;"))
+    private static int elytra(int original) {
+        return 22428;
+    }
+}
