eb616996126c0f216cc0989da913e98d9c6a39dc
PacManMVC <maxim.vancompernolle@gmail.com>
2025-04-19 18:57:05 -0400

    fr this time

--- src/main/java/draaft/draaft.java
+++ src/main/java/draaft/draaft.java
@@ -5,7 +5,7 @@ import net.fabricmc.api.ModInitializer;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
-public class drAAft implements ModInitializer {
+public class draaft implements ModInitializer {
 	public static final String MOD_ID = "draaft";
 	public static final Logger LOGGER = LogManager.getLogger(MOD_ID);
 
