b794e1938045d02284b45dc070ec0bbfb3a4605c
PacManMVC <maxim.vancompernolle@gmail.com>
2025-04-19 21:03:17 -0400

    dragon node height for zero standardized

--- src/main/java/draaft/mixin/HoldingPatternPhaseMixin.java
+++ src/main/java/draaft/mixin/HoldingPatternPhaseMixin.java
@@ -2,20 +2,20 @@ package draaft.mixin;
 
 import net.minecraft.entity.boss.dragon.EnderDragonEntity;
 import net.minecraft.entity.boss.dragon.phase.AbstractPhase;
-import net.minecraft.entity.boss.dragon.phase.StrafePlayerPhase;
+import net.minecraft.entity.boss.dragon.phase.HoldingPatternPhase;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Redirect;
 
 import java.util.Random;
 
-@Mixin(StrafePlayerPhase.class)
-public abstract class StrafePlayerPhaseMixin extends AbstractPhase {
-    public StrafePlayerPhaseMixin(EnderDragonEntity dragon) {
+@Mixin(HoldingPatternPhase.class)
+public abstract class HoldingPatternPhaseMixin extends AbstractPhase {
+    public HoldingPatternPhaseMixin(EnderDragonEntity dragon) {
         super(dragon);
     }
 
-    @Redirect(method = "method_6861", at = @At(value = "INVOKE", target = "Ljava/util/Random;nextFloat()F"))
+    @Redirect(method = "method_6842", at = @At(value = "INVOKE", target = "Ljava/util/Random;nextFloat()F"))
     private float injected(Random instance) {
         return 0.0F;
     }
