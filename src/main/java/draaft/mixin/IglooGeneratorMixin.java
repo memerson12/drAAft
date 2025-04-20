decf3a87c8ff841064782301f32af232e4a3812c
PacManMVC <maxim.vancompernolle@gmail.com>
2025-04-19 21:39:05 -0400

    all igloos have basements

---
+++ src/main/java/draaft/mixin/IglooGeneratorMixin.java
@@ -0,0 +1,16 @@
+package draaft.mixin;
+
+import net.minecraft.structure.IglooGenerator;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Redirect;
+
+import java.util.Random;
+
+@Mixin(IglooGenerator.class)
+public abstract class IglooGeneratorMixin {
+    @Redirect(method = "addPieces", at = @At(value = "INVOKE", target = "Ljava/util/Random;nextDouble()D"))
+    private static double injected(Random instance) {
+        return 0;
+    }
+}
