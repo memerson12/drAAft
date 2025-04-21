09908e5d61e62fe8b6bea442e248af2c9329a5de
PacManMVC <maxim.vancompernolle@gmail.com>
2025-04-20 23:57:38 -0400

    standardize dragon insta perch and 1 in 8

--- src/main/java/draaft/mixin/HoldingPatternPhaseMixin.java
+++ src/main/java/draaft/mixin/HoldingPatternPhaseMixin.java
@@ -16,7 +16,13 @@ public abstract class HoldingPatternPhaseMixin extends AbstractPhase {
     }
 
     @Redirect(method = "method_6842", at = @At(value = "INVOKE", target = "Ljava/util/Random;nextFloat()F"))
-    private float injected(Random instance) {
+    private float injectedFloat(Random instance) {
         return 0.0F;
+    }
+
+    @Redirect(method = "method_6841", at = @At(value = "INVOKE", target = "Ljava/util/Random;nextInt(I)I"))
+    private int injectedInt(Random instance, int i) {
+        instance.setSeed(this.dragon.getEntityWorld().getServer().getSaveProperties().getGeneratorOptions().getSeed());
+        return instance.nextInt(i);
     }
 }
