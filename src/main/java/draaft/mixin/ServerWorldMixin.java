9a5ee160acc10589342c17fd98c7c72a5c578f76
PacManMVC <maxim.vancompernolle@gmail.com>
2025-04-20 23:00:51 -0400

    real thunder rng standardization

--- src/main/java/draaft/mixin/ServerWorldMixin.java
+++ src/main/java/draaft/mixin/ServerWorldMixin.java
@@ -26,9 +26,9 @@ public abstract class ServerWorldMixin extends World implements ServerWorldAcces
     @Shadow @Final private MinecraftServer server;
 
     @Unique
-    boolean injectedThunder = false;
+    boolean lastTickThunder = true;
     @Unique
-    boolean injectedRain = false;
+    boolean lastTickRain = true;
 
     protected ServerWorldMixin(MutableWorldProperties mutableWorldProperties, RegistryKey<World> registryKey, RegistryKey<DimensionType> registryKey2, DimensionType dimensionType, Supplier<Profiler> profiler, boolean bl, boolean bl2, long l) {
         super(mutableWorldProperties, registryKey, registryKey2, dimensionType, profiler, bl, bl2, l);
@@ -37,29 +37,65 @@ public abstract class ServerWorldMixin extends World implements ServerWorldAcces
     @ModifyArg(method = "tick",
             at = @At(value = "INVOKE", target = "Lnet/minecraft/world/level/ServerWorldProperties;setThunderTime(I)V"))
     private int injectedThunder(int thunderTime) {
-        if (this.worldProperties.isThundering()) {
-            return thunderTime;
-        }
-        Random random = new Random(this.server.getSaveProperties().getGeneratorOptions().getSeed());
-        if (!injectedThunder) {
-            injectedThunder = true;
+        Random random = new Random(this.server.getSaveProperties().getGeneratorOptions().getSeed() + this.worldProperties.getTimeOfDay());
+
+        /*
+        TODO: BLACK MAGIC
+
+        if it just started not thundering, either because thunder ended OR we made the world
+        then make thunder time a seeded value 10-45 minutes
+        if it has been not thundering
+        then decrement by 1
+        if it just started thundering
+        then make thunder time a seeded value 3-13 minutes
+        if it has been thundering
+        then decrement by 1
+
+        for simplicity:
+        if current state is same as previous state
+        then decrement by 1
+         */
+        if ((lastTickThunder && this.worldProperties.getThunderTime() == 0 && !this.worldProperties.isThundering()) || this.worldProperties.getTime() == 0) {
+            lastTickThunder = false;
             return random.nextInt(42000) + 12000;
+        } else if (!lastTickThunder && this.worldProperties.getThunderTime() == 0 && this.worldProperties.isThundering()) {
+            lastTickThunder = true;
+            return random.nextInt(12000) + 3600;
         }
-        return thunderTime > 54000 ? random.nextInt(42000) + 12000 : thunderTime;
+
+        return thunderTime;
     }
 
     @ModifyArg(method = "tick",
             at = @At(value = "INVOKE", target = "Lnet/minecraft/world/level/ServerWorldProperties;setRainTime(I)V"))
     private int injectedRain(int rainTime) {
-        if (this.worldProperties.isRaining()) {
-            return rainTime;
+        Random random = new Random(this.server.getSaveProperties().getGeneratorOptions().getSeed() + this.worldProperties.getTimeOfDay());
+
+        /*
+        TODO: BLACK MAGIC
+
+        if it just started not raining, either because rain ended OR we made the world
+        then make rain time a seeded value 1-7 minutes less than thunder time
+        if it has been not raining
+        then decrement by 1
+        if it just started raining
+        then make rain time a seeded value 10-20 minutes
+        if it has been raining
+        then decrement by 1
+
+        for simplicity:
+        if current state is same as previous state
+        then decrement by 1
+         */
+        if ((lastTickRain && this.worldProperties.getRainTime() == 0 && !this.worldProperties.isRaining()) || this.worldProperties.getTime() == 0) {
+            lastTickRain = false;
+            int thunderTime = this.worldProperties.getThunderTime();
+            return thunderTime - (random.nextInt(7200) + 1200);
+        } else if (!lastTickRain && this.worldProperties.getRainTime() == 0 && this.worldProperties.isRaining()) {
+            lastTickRain = true;
+            return random.nextInt(12000) + 12000;
         }
-        Random random = new Random(this.server.getSaveProperties().getGeneratorOptions().getSeed());
-        int thunderTime = this.worldProperties.getThunderTime();
-        if (!injectedRain) {
-            injectedRain = true;
-            return (thunderTime - (random.nextInt(7200) + 1200));
-        }
-        return rainTime > 54000 ? (thunderTime - (random.nextInt(7200) + 1200)) : rainTime;
+
+        return rainTime;
     }
 }
