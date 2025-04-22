8758c4057958e8c9e19be1c90ce1a0d333f22e10
PacManMVC <maxim.vancompernolle@gmail.com>
2025-04-21 22:13:06 -0400

    drowned equipment and endermite rng are now seed based

--- src/main/java/draaft/mixin/DrownedEntityMixin.java
+++ src/main/java/draaft/mixin/DrownedEntityMixin.java
@@ -6,8 +6,13 @@ import net.minecraft.entity.mob.DrownedEntity;
 import net.minecraft.entity.mob.ZombieEntity;
 import net.minecraft.world.World;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Unique;
+import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Constant;
 import org.spongepowered.asm.mixin.injection.ModifyConstant;
+import org.spongepowered.asm.mixin.injection.Redirect;
+
+import java.util.Random;
 
 @Mixin(DrownedEntity.class)
 public abstract class DrownedEntityMixin extends ZombieEntity implements RangedAttackMob {
@@ -20,12 +25,37 @@ public abstract class DrownedEntityMixin extends ZombieEntity implements RangedA
     }
 
     @ModifyConstant(method = "initEquipment", constant = @Constant(doubleValue = 0.9))
-    private double injected(double value) {
+    private double injectedEquipment(double value) {
         return 0.7;
     }
 
     @ModifyConstant(method = "initialize", constant = @Constant(floatValue = 0.03F))
-    private float injected(float value) {
+    private float injectedShell(float value) {
         return 0.09F;
+    }
+
+    @Redirect(method = "initEquipment", at = @At(value = "INVOKE", target = "Ljava/util/Random;nextFloat()F"))
+    private float injectedEquipmentFloat(Random instance) {
+        instance.setSeed(getBlockSeed(instance, this.world.getServer().getSaveProperties().getGeneratorOptions().getSeed(), this.getBlockPos().getX(), this.getBlockPos().getZ()));
+        return instance.nextFloat();
+    }
+
+    @Redirect(method = "initEquipment", at = @At(value = "INVOKE", target = "Ljava/util/Random;nextInt(I)I"))
+    private int injectedEquipmentInt(Random instance, int i) {
+        return (int) getBlockSeed(instance, this.world.getServer().getSaveProperties().getGeneratorOptions().getSeed(), this.getBlockPos().getX(), this.getBlockPos().getZ());
+    }
+
+    @Redirect(method = "initialize", at = @At(value = "INVOKE", target = "Ljava/util/Random;nextFloat()F"))
+    private float injectedShell(Random instance) {
+        instance.setSeed(getBlockSeed(instance, this.world.getServer().getSaveProperties().getGeneratorOptions().getSeed(), this.getBlockPos().getX(), this.getBlockPos().getZ()));
+        return 1.0F - instance.nextFloat();
+    }
+
+    @Unique
+    private static long getBlockSeed(Random random, long seed, int x, int z) {
+        random.setSeed(seed);
+        long l = random.nextLong() | 1L;
+        long m = random.nextLong() | 1L;
+        return (long) x * l + (long) z * m ^ seed;
     }
 }
--- src/main/java/draaft/mixin/EnderPearlEntityMixin.java
+++ src/main/java/draaft/mixin/EnderPearlEntityMixin.java
@@ -1,13 +1,18 @@
 package draaft.mixin;
 
+import net.minecraft.entity.Entity;
 import net.minecraft.entity.EntityType;
 import net.minecraft.entity.LivingEntity;
 import net.minecraft.entity.projectile.thrown.EnderPearlEntity;
 import net.minecraft.entity.projectile.thrown.ThrownItemEntity;
+import net.minecraft.item.Items;
+import net.minecraft.server.network.ServerPlayerEntity;
+import net.minecraft.stat.Stats;
 import net.minecraft.world.World;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.injection.Constant;
-import org.spongepowered.asm.mixin.injection.ModifyConstant;
+import org.spongepowered.asm.mixin.injection.*;
+
+import java.util.Random;
 
 @Mixin(EnderPearlEntity.class)
 public abstract class EnderPearlEntityMixin extends ThrownItemEntity {
@@ -26,5 +31,21 @@ public abstract class EnderPearlEntityMixin extends ThrownItemEntity {
     @ModifyConstant(method = "onCollision", constant = @Constant(floatValue = 0.05F))
     private float injected(float constant) {
         return 0.07F;
+    }
+
+    @Redirect(method = "onCollision", at = @At(value = "INVOKE", target = "Ljava/util/Random;nextFloat()F"))
+    private float injected(Random instance) {
+        Entity entity = this.getOwner();
+        int pearlsUsed = 0;
+        if (entity instanceof ServerPlayerEntity) {
+            ServerPlayerEntity serverPlayerEntity = (ServerPlayerEntity) entity;
+            pearlsUsed = serverPlayerEntity.getStatHandler().getStat(Stats.USED.getOrCreateStat(Items.ENDER_PEARL));
+        }
+        long seed = this.world.getServer().getSaveProperties().getGeneratorOptions().getSeed();
+        instance.setSeed(seed);
+        long l = random.nextLong() | 1L;
+        long m = random.nextLong() | 1L;
+        instance.setSeed((long) pearlsUsed * l + (long) pearlsUsed * m ^ seed);
+        return instance.nextFloat();
     }
 }
