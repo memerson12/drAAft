bd50d44a5e87b9ddb9441d8d3099b3863c65ec5e
Michael <47831816+memerson12@users.noreply.github.com>
2025-04-29 23:56:09 -0700

    fix trident durability bug

--- src/main/java/draaft/mixin/ItemsMixin.java
+++ src/main/java/draaft/mixin/ItemsMixin.java
@@ -8,9 +8,17 @@ import org.spongepowered.asm.mixin.injection.Slice;
 
 @Mixin(Items.class)
 public class ItemsMixin {
-    @ModifyArg(method = "<clinit>",
-            slice = @Slice(from = @At(value = "CONSTANT", args = "stringValue=elytra")),
-            at = @At(value = "INVOKE", target = "Lnet/minecraft/item/Item$Settings;maxDamage(I)Lnet/minecraft/item/Item$Settings;"))
+    @ModifyArg(
+            method = "<clinit>",
+            slice = @Slice(
+                    from = @At(value = "NEW", target = "net/minecraft/item/ElytraItem"),
+                    to = @At(value = "INVOKE", target = "net/minecraft/item/ElytraItem.<init>(Lnet/minecraft/item/Item$Settings;)V")
+            ),
+            at = @At(
+                    value = "INVOKE",
+                    target = "Lnet/minecraft/item/Item$Settings;maxDamage(I)Lnet/minecraft/item/Item$Settings;"
+            )
+    )
     private static int elytra(int original) {
         return 22824;
     }
