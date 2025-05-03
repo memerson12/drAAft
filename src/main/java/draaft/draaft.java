aa1f9336338cc4458c88dbb1a87fd8bc1698bd82
Michael <47831816+memerson12@users.noreply.github.com>
2025-05-03 13:22:44 -0700

    make mod version available in java code

--- src/main/java/draaft/draaft.java
+++ src/main/java/draaft/draaft.java
@@ -4,13 +4,31 @@ import net.fabricmc.api.ModInitializer;
 
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
+import java.io.InputStream;
+import java.util.Properties;
 
 public class draaft implements ModInitializer {
 	public static final String MOD_ID = "draaft";
 	public static final Logger LOGGER = LogManager.getLogger(MOD_ID);
+	public static final String DRAAFT_VERSION = getDraaftVersion();
 
 	@Override
 	public void onInitialize() {
-		LOGGER.info("AA Changes Initialized");
+        LOGGER.info("Draaft version: {}", DRAAFT_VERSION);
+	}
+	public static String getDraaftVersion() {
+		try (InputStream input = draaft.class.getClassLoader().getResourceAsStream("config.properties")) {
+			Properties prop = new Properties();
+			prop.load(input);
+            String version = prop.getProperty("DRAAFT_VERSION");
+			if(version == null) {
+				LOGGER.warn("Could not get draaft version");
+				return "Unknown";
+			}
+			return version;
+		} catch (Exception e) {
+			LOGGER.error("Failed to load config.properties", e);
+			return "Unknown";
+		}
 	}
 }
