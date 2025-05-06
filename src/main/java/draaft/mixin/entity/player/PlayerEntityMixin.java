0364f9dd4241070a55c972aa6852844e70937c94
Michael <47831816+memerson12@users.noreply.github.com>
2025-05-05 18:10:52 -0700

    maybe dragon fix

--- src/main/java/draaft/mixin/entity/player/PlayerEntityMixin.java
+++ src/main/java/draaft/mixin/entity/player/PlayerEntityMixin.java
@@ -2,10 +2,13 @@ package draaft.mixin.entity.player;
 
 import com.mojang.authlib.GameProfile;
 import draaft.api.PlayerEntityAccessor;
+import draaft.persistent.WorldState;
 import net.minecraft.entity.EntityType;
 import net.minecraft.entity.LivingEntity;
 import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.nbt.CompoundTag;
 import net.minecraft.server.MinecraftServer;
+import net.minecraft.server.world.ServerWorld;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.World;
 import org.spongepowered.asm.mixin.Mixin;
@@ -14,6 +17,11 @@ import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
 import java.util.Random;
 
 @Mixin(PlayerEntity.class)
@@ -28,6 +36,10 @@ public abstract class PlayerEntityMixin extends LivingEntity implements PlayerEn
     @Override
     public void draaft$setPearlRandom(Random random) {
         this.draaft_persistentPearlRng = random;
+        if (this.world instanceof ServerWorld) {
+            WorldState state = WorldState.getServerState((ServerWorld) this.world);
+            state.setPearlRng(random);
+        }
     }
 
     @Override
@@ -38,8 +50,52 @@ public abstract class PlayerEntityMixin extends LivingEntity implements PlayerEn
     @Inject(method = "<init>", at = @At("RETURN"))
     private void draaft_onInit(World world, BlockPos blockPos, GameProfile gameProfile, CallbackInfo ci) {
         MinecraftServer server = world.getServer();
-        if (server == null) return;
+        if (server == null)
+            return;
         long seed = server.getSaveProperties().getGeneratorOptions().getSeed();
-        this.draaft_persistentPearlRng = new Random(seed);
+
+        if (world instanceof ServerWorld) {
+            WorldState state = WorldState.getServerState((ServerWorld) world);
+            Random storedRng = state.getPearlRng();
+            if (storedRng != null) {
+                this.draaft_persistentPearlRng = storedRng;
+            } else {
+                this.draaft_persistentPearlRng = new Random(seed);
+                state.setPearlRng(this.draaft_persistentPearlRng);
+            }
+        } else {
+            this.draaft_persistentPearlRng = new Random(seed);
+        }
+    }
+
+    @Inject(method = "writeCustomDataToTag", at = @At("RETURN"))
+    private void draaft_writeTag(CompoundTag tag, CallbackInfo ci) {
+        if (this.draaft_persistentPearlRng != null) {
+            try (ByteArrayOutputStream baos = new ByteArrayOutputStream();
+                    ObjectOutputStream oos = new ObjectOutputStream(baos)) {
+                oos.writeObject(this.draaft_persistentPearlRng);
+                tag.putByteArray("draaft_pearl_rng", baos.toByteArray());
+            } catch (IOException e) {
+                // If serialization fails, fall back to just storing the seed
+                tag.putLong("draaft_pearl_seed", this.draaft_persistentPearlRng.nextLong());
+            }
+        }
+    }
+
+    @Inject(method = "readCustomDataFromTag", at = @At("RETURN"))
+    private void draaft_readTag(CompoundTag tag, CallbackInfo ci) {
+        if (tag.contains("draaft_pearl_rng")) {
+            try (ByteArrayInputStream bais = new ByteArrayInputStream(tag.getByteArray("draaft_pearl_rng"));
+                    ObjectInputStream ois = new ObjectInputStream(bais)) {
+                this.draaft_persistentPearlRng = (Random) ois.readObject();
+            } catch (IOException | ClassNotFoundException e) {
+                // If deserialization fails, fall back to just using the seed
+                if (tag.contains("draaft_pearl_seed")) {
+                    this.draaft_persistentPearlRng = new Random(tag.getLong("draaft_pearl_seed"));
+                }
+            }
+        } else if (tag.contains("draaft_pearl_seed")) {
+            this.draaft_persistentPearlRng = new Random(tag.getLong("draaft_pearl_seed"));
+        }
     }
 }
---
+++ src/main/java/draaft/persistent/WorldState.java
@@ -0,0 +1,73 @@
+package draaft.persistent;
+
+import net.minecraft.nbt.CompoundTag;
+import net.minecraft.server.world.ServerWorld;
+import net.minecraft.world.PersistentState;
+
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.util.Random;
+
+public class WorldState extends PersistentState {
+    private long test;
+    private Random pearlRng;
+
+    public WorldState(String key) {
+        super(key);
+        this.test = 0L;
+    }
+
+    public static WorldState getServerState(ServerWorld world) {
+        return world.getPersistentStateManager().getOrCreate(() -> new WorldState("draaft_world_state"),
+                "draaft_world_state");
+    }
+
+    @Override
+    public void fromTag(CompoundTag tag) {
+        this.test = tag.getLong("test");
+
+        if (tag.contains("pearl_rng")) {
+            try (ByteArrayInputStream bais = new ByteArrayInputStream(tag.getByteArray("pearl_rng"));
+                    ObjectInputStream ois = new ObjectInputStream(bais)) {
+                this.pearlRng = (Random) ois.readObject();
+            } catch (IOException | ClassNotFoundException e) {
+                // If deserialization fails, fall back to just using the seed
+                if (tag.contains("pearl_seed")) {
+                    this.pearlRng = new Random(tag.getLong("pearl_seed"));
+                }
+            }
+        } else if (tag.contains("pearl_seed")) {
+            this.pearlRng = new Random(tag.getLong("pearl_seed"));
+        }
+    }
+
+    @Override
+    public CompoundTag toTag(CompoundTag nbt) {
+        nbt.putLong("test", this.test);
+
+        if (this.pearlRng != null) {
+            try (ByteArrayOutputStream baos = new ByteArrayOutputStream();
+                    ObjectOutputStream oos = new ObjectOutputStream(baos)) {
+                oos.writeObject(this.pearlRng);
+                nbt.putByteArray("pearl_rng", baos.toByteArray());
+            } catch (IOException e) {
+                // If serialization fails, fall back to just storing the seed
+                nbt.putLong("pearl_seed", this.pearlRng.nextLong());
+            }
+        }
+
+        return nbt;
+    }
+
+    public Random getPearlRng() {
+        return this.pearlRng;
+    }
+
+    public void setPearlRng(Random rng) {
+        this.pearlRng = rng;
+        this.markDirty();
+    }
+}
