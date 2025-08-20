0f465ea49267133d7de4085d11195acc2d0cbfdb
DesktopFolder <desktopfolder@users.noreply.github.com>
2025-08-20 18:49:31 -0400

    Basic server auth setup

--- src/main/java/draaft/draaft.java
+++ src/main/java/draaft/draaft.java
@@ -1,7 +1,13 @@
 package draaft;
 
+import com.mojang.authlib.exceptions.AuthenticationException;
+import com.mojang.authlib.exceptions.AuthenticationUnavailableException;
+import com.mojang.authlib.exceptions.InvalidCredentialsException;
 import net.fabricmc.api.ModInitializer;
 
+import net.minecraft.client.MinecraftClient;
+import net.minecraft.client.util.Session;
+import net.minecraft.text.TranslatableText;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import java.io.InputStream;
@@ -12,9 +18,25 @@ public class draaft implements ModInitializer {
 	public static final Logger LOGGER = LogManager.getLogger(MOD_ID);
 	public static final String DRAAFT_VERSION = getDraaftVersion();
 
+	public void evilConnectionTesting(/* :) */) {
+		MinecraftClient inst = MinecraftClient.getInstance();
+		Session session = inst.getSession();
+		try {
+			inst.getSessionService().joinServer(session.getProfile(), session.getAccessToken(), "draaft2025server");
+			LOGGER.info("draaft successfully joined server draaft2025server");
+		} catch (AuthenticationUnavailableException var3) {
+			LOGGER.warn("disconnect.loginFailedInfo: disconnect.loginFailedInfo.serversUnavailable");
+		} catch (InvalidCredentialsException var4) {
+			LOGGER.warn("disconnect.loginFailedInfo: disconnect.loginFailedInfo.invalidSession");
+		} catch (AuthenticationException authenticationException) {
+			LOGGER.warn("disconnect.loginFailedInfo {}", authenticationException.getMessage());
+		}
+	}
+
 	@Override
 	public void onInitialize() {
-        LOGGER.info("Draaft version: {}", DRAAFT_VERSION);
+		LOGGER.info("Draaft version: {}", DRAAFT_VERSION);
+		evilConnectionTesting();
 	}
 	public static String getDraaftVersion() {
 		try (InputStream input = draaft.class.getClassLoader().getResourceAsStream("config.properties")) {
