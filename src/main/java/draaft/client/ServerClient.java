9d46e2bf25ae23a50e14bb0a024f3542dba4e98b
DesktopFolder <desktopfolder@users.noreply.github.com>
2025-08-28 21:41:43 -0400

    Net: Add basics

--- src/main/java/draaft/client/ServerClient.java
+++ src/main/java/draaft/client/ServerClient.java
@@ -1,15 +1,22 @@
 package draaft.client;
 
+import com.google.gson.Gson;
+import com.google.gson.JsonObject;
 import com.mojang.authlib.exceptions.AuthenticationException;
 import com.mojang.authlib.exceptions.AuthenticationUnavailableException;
 import com.mojang.authlib.exceptions.InvalidCredentialsException;
 import net.minecraft.client.MinecraftClient;
 import net.minecraft.client.util.Session;
 import org.apache.commons.codec.binary.Base32;
-import org.apache.http.client.HttpClient;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
+import java.io.IOException;
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
 import java.security.NoSuchAlgorithmException;
 import java.security.SecureRandom;
 
@@ -17,7 +24,9 @@ import static draaft.draaft.MOD_ID;
 
 public class ServerClient {
     private String clientPassword = null;
+    private String clientToken = null;
     public static final Logger LOGGER = LogManager.getLogger(MOD_ID);
+    HttpClient httpClient = HttpClient.newBuilder().version(HttpClient.Version.HTTP_1_1).build();
 
     public ServerClient() {
         SecureRandom instanceStrong = null;
@@ -26,9 +35,9 @@ public class ServerClient {
         } catch (NoSuchAlgorithmException e) {
             throw new RuntimeException(e);
         }
-        byte[] randomBytes = new byte[20];
+        byte[] randomBytes = new byte[15];
         instanceStrong.nextBytes(randomBytes);
-        clientPassword = new Base32().encodeAsString(randomBytes);
+        clientPassword = new Base32().encodeAsString(randomBytes) + "draaaaft";
     }
 
     // thanks menx :)
@@ -40,13 +49,43 @@ public class ServerClient {
 
         try {
             inst.getSessionService().joinServer(session.getProfile(), session.getAccessToken(), clientPassword);
-            LOGGER.info("draaft successfully joined server draaft2025server");
+            LOGGER.info("draaft successfully joined server with clientPassword");
         } catch (AuthenticationUnavailableException var3) {
             LOGGER.warn("disconnect.loginFailedInfo: disconnect.loginFailedInfo.serversUnavailable");
         } catch (InvalidCredentialsException var4) {
             LOGGER.warn("disconnect.loginFailedInfo: disconnect.loginFailedInfo.invalidSession");
         } catch (AuthenticationException authenticationException) {
             LOGGER.warn("disconnect.loginFailedInfo {}", authenticationException.getMessage());
+        }
+        String username = session.getUsername();
+
+        JsonObject body = new JsonObject();
+        body.addProperty("serverID", clientPassword);
+        body.addProperty("username", username);
+
+        try {
+            var req = HttpRequest.newBuilder(new URI("http://localhost:8000/authenticate"))
+                    .POST(HttpRequest.BodyPublishers.ofString(body.toString()))
+                    .setHeader("Content-Type", "application/json")
+                    .build();
+            HttpResponse<String> resp = httpClient.send(req, HttpResponse.BodyHandlers.ofString());
+            JsonObject result  = new Gson().fromJson(resp.body(), JsonObject.class);
+            clientToken = result.get("token").getAsString();
+        } catch (IOException | InterruptedException | URISyntaxException e) {
+            throw new RuntimeException(e);
+        }
+
+        try {
+            var req = HttpRequest.newBuilder(new URI("http://localhost:8000/room/create"))
+                    .GET()
+                    .setHeader("Content-Type", "application/json")
+                    .setHeader("token", clientToken)
+                    .build();
+            HttpResponse<String> resp = httpClient.send(req, HttpResponse.BodyHandlers.ofString());
+            System.out.println("----- YOUR DRAAFT ROOM CODE! READY FOR JOINING :) (in 2027)");
+            System.out.println(resp.body());
+        } catch (IOException | InterruptedException | URISyntaxException e) {
+            throw new RuntimeException(e);
         }
     }
 }
