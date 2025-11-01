33e9cf19f0512a30304c663cbc04c1ae68238167
me-nx <me0nx@proton.me>
2025-11-01 17:55:36 +0200

    Change peaceful revert behaviour, SpeedrunAPI config
    On peaceful switch to the previously set difficulty instead of hard.

--- src/main/java/draaft/mixin/client/gui/screen/options/OptionsScreenMixin.java
+++ src/main/java/draaft/mixin/client/gui/screen/options/OptionsScreenMixin.java
@@ -1,6 +1,8 @@
 package draaft.mixin.client.gui.screen.options;
 
+import draaft.client.ModConfig;
 import net.minecraft.client.MinecraftClient;
+import net.minecraft.client.gui.screen.Screen;
 import net.minecraft.client.gui.screen.options.OptionsScreen;
 import net.minecraft.client.gui.widget.ButtonWidget;
 import net.minecraft.network.packet.c2s.play.UpdateDifficultyC2SPacket;
@@ -15,38 +17,61 @@ import org.spongepowered.asm.mixin.injection.Redirect;
 import java.util.Objects;
 
 @Mixin(OptionsScreen.class)
-public abstract class OptionsScreenMixin {
+public abstract class OptionsScreenMixin extends Screen {
+    @Unique
+    private final OptionsScreenAccessor self = (OptionsScreenAccessor)this;
+
+    @Unique
+    private Difficulty originalDifficulty;
+
     @Unique
     private boolean pseudoPeacefulSelected = false;
 
+    private OptionsScreenMixin(Text title) {
+        super(title);
+    }
+
     @Redirect(method = "init", at = @At(value = "NEW", ordinal = 0, target = "Lnet/minecraft/client/gui/widget/ButtonWidget;"))
     ButtonWidget difficultyButton(int x, int y, int width, int height, Text message, ButtonWidget.PressAction onPress) {
-        var self = (OptionsScreenAccessor)this;
+        if (!ModConfig.getInstance().disablePeaceful) {
+            return new ButtonWidget(x, y, width, height, message, onPress);
+        }
+
+        // this instance is reused when one of the option subscreens is closed
+        if (!this.pseudoPeacefulSelected) {
+            this.originalDifficulty = self.draaft$difficulty();
+        } else {
+            this.pseudoPeacefulSelected = false;
+        }
 
         return new ButtonWidget(x, y, width, height, message, (btn) -> {
-            if (self.draaft$difficulty() == Difficulty.HARD) {
-                pseudoPeacefulSelected = !pseudoPeacefulSelected;
-
-                if (!pseudoPeacefulSelected) {
-                    // PEACEFUL + 1 = EASY
-                    self.draaft$setDifficulty(Difficulty.PEACEFUL);
-                }
-            }
-
-            if (!pseudoPeacefulSelected) {
-                self.draaft$setDifficulty(Difficulty.byOrdinal(self.draaft$difficulty().getId() + 1));
-
-                Objects.requireNonNull(MinecraftClient.getInstance().getNetworkHandler())
-                    .sendPacket(new UpdateDifficultyC2SPacket(self.draaft$difficulty()));
+            if (this.pseudoPeacefulSelected) {
+                this.switchTo(Difficulty.EASY);
+                this.pseudoPeacefulSelected = false;
 
                 self.draaft$difficultyButton().setMessage(self.draaft$getDifficultyButtonText(self.draaft$difficulty()));
-            } else {
+            } else if (self.draaft$difficulty() == Difficulty.HARD) {
+                this.switchTo(this.originalDifficulty);
+                this.pseudoPeacefulSelected = true;
+
                 self.draaft$difficultyButton().setMessage(
                     new TranslatableText("options.difficulty")
                         .append(": ")
                         .append(new TranslatableText("draaft.game.options.pseudoPeaceful"))
                 );
+            } else {
+                this.switchTo(Difficulty.byOrdinal(self.draaft$difficulty().getId() + 1));
+
+                self.draaft$difficultyButton().setMessage(self.draaft$getDifficultyButtonText(self.draaft$difficulty()));
             }
         });
+    }
+
+    @Unique
+    private void switchTo(Difficulty difficulty) {
+        self.draaft$setDifficulty(difficulty);
+
+        Objects.requireNonNull(MinecraftClient.getInstance().getNetworkHandler())
+            .sendPacket(new UpdateDifficultyC2SPacket(difficulty));
     }
 }
