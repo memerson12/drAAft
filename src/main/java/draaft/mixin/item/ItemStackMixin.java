9c7ed0fec88f5b6541e59aa2a4d513d944c346b5
me-nx <me0nx@proton.me>
2025-12-08 21:45:33 +0200

    Lazy static enchantment map (#30)

--- src/main/java/draaft/mixin/item/ItemStackMixin.java
+++ src/main/java/draaft/mixin/item/ItemStackMixin.java
@@ -8,9 +8,11 @@ import net.minecraft.item.ItemStack;
 import net.minecraft.nbt.CompoundTag;
 import net.minecraft.nbt.ListTag;
 import net.minecraft.nbt.Tag;
+import net.minecraft.util.Lazy;
 import net.minecraft.util.registry.Registry;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
@@ -20,6 +22,42 @@ import java.util.Map;
 
 @Mixin(ItemStack.class)
 public abstract class ItemStackMixin {
+    @Unique private final static Lazy<List<Enchantment>> pickaxeshovelEnchants = new Lazy<>(() -> List.of(Enchantments.EFFICIENCY, Enchantments.FORTUNE, Enchantments.MENDING, Enchantments.UNBREAKING));
+    @Unique private final static Lazy<List<Enchantment>> axehoeEnchants = new Lazy<>(() -> List.of(Enchantments.EFFICIENCY, Enchantments.MENDING, Enchantments.SILK_TOUCH, Enchantments.UNBREAKING));
+    @Unique private final static Lazy<List<Enchantment>> swordEnchants = new Lazy<>(() -> List.of(Enchantments.FIRE_ASPECT, Enchantments.KNOCKBACK, Enchantments.LOOTING, Enchantments.MENDING, Enchantments.SMITE, Enchantments.SWEEPING, Enchantments.UNBREAKING));
+    @Unique private final static Lazy<List<Enchantment>> helmetEnchants = new Lazy<>(() -> List.of(Enchantments.AQUA_AFFINITY, Enchantments.MENDING, Enchantments.PROTECTION, Enchantments.RESPIRATION, Enchantments.UNBREAKING));
+    @Unique private final static Lazy<List<Enchantment>> chestplateleggingsEnchants = new Lazy<>(() -> List.of(Enchantments.MENDING, Enchantments.PROTECTION, Enchantments.UNBREAKING));
+    @Unique private final static Lazy<List<Enchantment>> bootsEnchants = new Lazy<>(() -> List.of(Enchantments.DEPTH_STRIDER, Enchantments.FEATHER_FALLING, Enchantments.MENDING, Enchantments.PROTECTION, Enchantments.SOUL_SPEED, Enchantments.UNBREAKING));
+    @Unique private final static Lazy<List<Enchantment>> compassEnchants = new Lazy<>(() -> List.of(Enchantments.VANISHING_CURSE));
+    @Unique private final static Lazy<List<Enchantment>> fnsoasEnchants = new Lazy<>(() -> List.of(Enchantments.MENDING, Enchantments.UNBREAKING));
+    @Unique private final static Lazy<List<Enchantment>> rodEnchants = new Lazy<>(() -> List.of(Enchantments.LUCK_OF_THE_SEA, Enchantments.LURE, Enchantments.MENDING, Enchantments.UNBREAKING));
+    @Unique private final static Lazy<List<Enchantment>> bowEnchants = new Lazy<>(() -> List.of(Enchantments.FLAME, Enchantments.INFINITY, Enchantments.POWER, Enchantments.PUNCH, Enchantments.UNBREAKING));
+    @Unique private final static Lazy<List<Enchantment>> tridentEnchants = new Lazy<>(() -> List.of(Enchantments.CHANNELING, Enchantments.IMPALING, Enchantments.LOYALTY, Enchantments.PUNCH, Enchantments.UNBREAKING));
+    @Unique private final static Lazy<List<Enchantment>> crossbowEnchants = new Lazy<>(() -> List.of(Enchantments.PIERCING, Enchantments.MENDING, Enchantments.QUICK_CHARGE, Enchantments.UNBREAKING));
+
+    @Unique
+    private static final Lazy<Map<String, List<Enchantment>>> enchantmentMap = new Lazy<>(() -> Map.ofEntries(
+        Map.entry("pickaxe", pickaxeshovelEnchants.get()),
+        Map.entry("shovel", pickaxeshovelEnchants.get()),
+        Map.entry("_axe", axehoeEnchants.get()),
+        Map.entry("hoe", axehoeEnchants.get()),
+        Map.entry("shears", axehoeEnchants.get()),
+        Map.entry("sword", swordEnchants.get()),
+        Map.entry("helmet", helmetEnchants.get()),
+        Map.entry("chestplate", chestplateleggingsEnchants.get()),
+        Map.entry("leggings", chestplateleggingsEnchants.get()),
+        Map.entry("boots", bootsEnchants.get()),
+        Map.entry("compass", compassEnchants.get()),
+        Map.entry("flint_and_steel", fnsoasEnchants.get()),
+        Map.entry("_on_a_stick", fnsoasEnchants.get()),
+        Map.entry("shield", fnsoasEnchants.get()),
+        Map.entry("elytra", fnsoasEnchants.get()),
+        Map.entry("fishing_rod", rodEnchants.get()),
+        Map.entry("bow", bowEnchants.get()),
+        Map.entry("trident", tridentEnchants.get()),
+        Map.entry("crossbow", crossbowEnchants.get())
+    ));
+
     @Shadow
     public abstract void putSubTag(String key, Tag tag);
 
@@ -29,25 +67,11 @@ public abstract class ItemStackMixin {
             return;
         }
 
-        List<Enchantment> pickaxeshovelEnchants = List.of(Enchantments.EFFICIENCY, Enchantments.FORTUNE, Enchantments.MENDING, Enchantments.UNBREAKING);
-        List<Enchantment> axehoeEnchants = List.of(Enchantments.EFFICIENCY, Enchantments.MENDING, Enchantments.SILK_TOUCH, Enchantments.UNBREAKING);
-        List<Enchantment> swordEnchants = List.of(Enchantments.FIRE_ASPECT, Enchantments.KNOCKBACK, Enchantments.LOOTING, Enchantments.MENDING, Enchantments.SMITE, Enchantments.SWEEPING, Enchantments.UNBREAKING);
-        List<Enchantment> helmetEnchants = List.of(Enchantments.AQUA_AFFINITY, Enchantments.MENDING, Enchantments.PROTECTION, Enchantments.RESPIRATION, Enchantments.UNBREAKING);
-        List<Enchantment> chestplateleggingsEnchants = List.of(Enchantments.MENDING, Enchantments.PROTECTION, Enchantments.UNBREAKING);
-        List<Enchantment> bootsEnchants = List.of(Enchantments.DEPTH_STRIDER, Enchantments.FEATHER_FALLING, Enchantments.MENDING, Enchantments.PROTECTION, Enchantments.SOUL_SPEED, Enchantments.UNBREAKING);
-        List<Enchantment> compassEnchants = List.of(Enchantments.VANISHING_CURSE);
-        List<Enchantment> fnsoasEnchants = List.of(Enchantments.MENDING, Enchantments.UNBREAKING);
-        List<Enchantment> rodEnchants = List.of(Enchantments.LUCK_OF_THE_SEA, Enchantments.LURE, Enchantments.MENDING, Enchantments.UNBREAKING);
-        List<Enchantment> bowEnchants = List.of(Enchantments.FLAME, Enchantments.INFINITY, Enchantments.POWER, Enchantments.PUNCH, Enchantments.UNBREAKING);
-        List<Enchantment> tridentEnchants = List.of(Enchantments.CHANNELING, Enchantments.IMPALING, Enchantments.LOYALTY, Enchantments.PUNCH, Enchantments.UNBREAKING);
-        List<Enchantment> crossbowEnchants = List.of(Enchantments.PIERCING, Enchantments.MENDING, Enchantments.QUICK_CHARGE, Enchantments.UNBREAKING);
-        Map<String, List<Enchantment>> enchantmentMap = Map.ofEntries(Map.entry("pickaxe", pickaxeshovelEnchants), Map.entry("shovel", pickaxeshovelEnchants), Map.entry("_axe", axehoeEnchants), Map.entry("hoe", axehoeEnchants), Map.entry("shears", axehoeEnchants), Map.entry("sword", swordEnchants), Map.entry("helmet", helmetEnchants), Map.entry("chestplate", chestplateleggingsEnchants), Map.entry("leggings", chestplateleggingsEnchants), Map.entry("boots", bootsEnchants), Map.entry("compass", compassEnchants), Map.entry("flint_and_steel", fnsoasEnchants), Map.entry("_on_a_stick", fnsoasEnchants), Map.entry("shield", fnsoasEnchants), Map.entry("elytra", fnsoasEnchants), Map.entry("fishing_rod", rodEnchants), Map.entry("bow", bowEnchants), Map.entry("trident", tridentEnchants), Map.entry("crossbow", crossbowEnchants));
-
         if (item != null && !item.asItem().getTranslationKey().contains("bowl")) {
-            for (String id : enchantmentMap.keySet()) {
+            for (String id : enchantmentMap.get().keySet()) {
                 if (item.asItem().getTranslationKey().contains(id)) {
                     ListTag listTag = new ListTag();
-                    for (Enchantment enchantment : enchantmentMap.get(id)) {
+                    for (Enchantment enchantment : enchantmentMap.get().get(id)) {
                         CompoundTag compoundTag = new CompoundTag();
                         compoundTag.putString("id", String.valueOf(Registry.ENCHANTMENT.getId(enchantment)));
                         compoundTag.putShort("lvl", (short) 1);
