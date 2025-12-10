db8adf9c4a8ba92b22b7664d2aeb928bb5b357d8
PacManMVC <maxim.vancompernolle@gmail.com>
2025-12-09 22:23:33 -0500

    i hate this desktopfolder guy

--- src/main/java/draaft/mixin/entity/player/PlayerEntityMixin.java
+++ src/main/java/draaft/mixin/entity/player/PlayerEntityMixin.java
@@ -1,13 +1,10 @@
 package draaft.mixin.entity.player;
 
-import com.mojang.authlib.GameProfile;
 import draaft.persistent.WorldManifest;
 import net.minecraft.entity.EntityType;
 import net.minecraft.entity.LivingEntity;
 import net.minecraft.entity.player.PlayerEntity;
-import net.minecraft.entity.projectile.thrown.ThrownItemEntity;
 import net.minecraft.server.world.ServerWorld;
-import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.World;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
@@ -24,7 +21,7 @@ public abstract class PlayerEntityMixin extends LivingEntity {
     float multiplyFallDamage(float fallDistance) {
         if (!world.isClient() && WorldManifest.get((ServerWorld) world).on(WorldManifest.Feature.DANGEROUS_PEARLS))
         {
-            return fallDistance + ((fallDistance - 3.f) * 3f);
+            return fallDistance + ((fallDistance - 3.0F) * 3.0F);
         }
         return fallDistance;
     }
