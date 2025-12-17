271857ac6092f22fd286697e57a388682867425f
PacManMVC <maxim.vancompernolle@gmail.com>
2025-12-17 13:32:06 -0500

    -1 warning

--- src/main/java/draaft/mixin/world/biome/source/BiomeSourceMixin.java
+++ src/main/java/draaft/mixin/world/biome/source/BiomeSourceMixin.java
@@ -4,17 +4,19 @@ import com.google.common.collect.Lists;
 import net.minecraft.world.biome.Biome;
 import net.minecraft.world.biome.Biomes;
 import net.minecraft.world.biome.source.BiomeSource;
-import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 import java.util.List;
 
 @Mixin(BiomeSource.class)
 public abstract class BiomeSourceMixin {
-    @Shadow
-    @Final
-    private final static List<Biome> SPAWN_BIOMES = Lists.<Biome>newArrayList(
-        Biomes.FOREST, Biomes.DESERT, Biomes.PLAINS, Biomes.TAIGA, Biomes.TAIGA_HILLS, Biomes.WOODED_HILLS, Biomes.JUNGLE, Biomes.JUNGLE_HILLS
-    );
+    @Inject(method = "getSpawnBiomes", at = @At("HEAD"), cancellable = true)
+    void getSpawnBiomes(CallbackInfoReturnable<List<Biome>> cir) {
+        cir.setReturnValue(Lists.newArrayList(
+            Biomes.FOREST, Biomes.DESERT, Biomes.PLAINS, Biomes.TAIGA, Biomes.TAIGA_HILLS, Biomes.WOODED_HILLS, Biomes.JUNGLE, Biomes.JUNGLE_HILLS
+        ));
+    }
 }
