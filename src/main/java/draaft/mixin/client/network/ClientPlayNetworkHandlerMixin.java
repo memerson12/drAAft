112d3c3d24c172e70f77669a58f3a64b3dcc6baa
me-nx <me0nx@proton.me>
2025-12-22 03:45:33 +0100

    Fix desync fake death with changed max health
    Link: https://bugs.mojang.com/browse/MC/issues/MC-19690

--- src/main/java/draaft/mixin/client/network/ClientPlayNetworkHandlerMixin.java
+++ src/main/java/draaft/mixin/client/network/ClientPlayNetworkHandlerMixin.java
@@ -1,8 +1,11 @@
 package draaft.mixin.client.network;
 
 import com.llamalad7.mixinextras.sugar.Local;
+import draaft.draaft;
+import draaft.network.HealthResetS2CPacket;
 import draaft.world.WorldInterface;
 import draaft.world.WorldClientInfo;
+import net.minecraft.client.MinecraftClient;
 import net.minecraft.client.network.ClientPlayNetworkHandler;
 import net.minecraft.client.world.ClientWorld;
 import net.minecraft.network.packet.s2c.play.CustomPayloadS2CPacket;
@@ -18,13 +21,25 @@ public class ClientPlayNetworkHandlerMixin {
     @Shadow
     private ClientWorld world;
 
+    @Shadow
+    private MinecraftClient client;
     @Unique
     private static final String LOGGER_WARN_TARGET = "Lorg/apache/logging/log4j/Logger;warn(Ljava/lang/String;Ljava/lang/Object;)V";
 
     @Redirect(method = "onCustomPayload", at = @At(value = "INVOKE", target = LOGGER_WARN_TARGET, remap = false))
     void handleDraaftPackets(Logger instance, String fmt, Object args, @Local(argsOnly = true) CustomPayloadS2CPacket packet) {
-        if (packet.getChannel().equals(WorldClientInfo.CHANNEL)) {
-            ((WorldInterface) world).draaft$setClientInfo(WorldClientInfo.fromBuffer(packet.getData()));
+        var channel = packet.getChannel();
+
+        if (channel.getNamespace().equals("draaft")) {
+            if (channel.equals(WorldClientInfo.CHANNEL)) {
+                ((WorldInterface) world).draaft$setClientInfo(WorldClientInfo.fromBuffer(packet.getData()));
+            } else if (channel.equals(HealthResetS2CPacket.CHANNEL)) {
+                if (this.client.player != null) {
+                    this.client.player.setHealth(HealthResetS2CPacket.fromBuffer(packet.getData()).health());
+                }
+            } else {
+                draaft.LOGGER.warn("unknown draaft packet");
+            }
         } else {
             instance.warn(fmt, args);
         }
